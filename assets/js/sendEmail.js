e778a16d01ff0d1344f0b113a8e0a2a42e19b8a8
Stephen Afolabi <stephenafolabi@gmail.com>
2018-12-15 23:36:21 +0000

    added portfolio and contact pages

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,20 @@
+function sendMail(contactForm) {
+    var service_id = "default_service";
+    var template_id = "heritage";
+    emailjs.send("gmail", "heritage", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value,
+    })
+        
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    contactForm.reset();
+    return false;  // To block from loading a new page
+}
